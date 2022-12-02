901c3434f14c0dbd2caffabbb5874e121b919b86
Anto462 <0anthony0402@gmail.com>
2022-12-02 00:03:53 -0600

    Se continua con el index y se ajusta la BD

--- DB.sql
+++ DB.sql
@@ -1,56 +1,70 @@
+---En aquellos donde se puntue coloque number para darles valores del 1 al 5
+
+create table proveedor(
+id_proveedor nvarchar(10) not NULL
+,nombre varchar(25)
+,email varchar (25)
+,fiabilidad number
+,localizacion varchar(25)
+,producto varchar (25)
+,CONSTRAINT proveedor_pk primary key (id_proveedor)
+)
+
+create table Usuario(
+id_usuario nvarchar(10) not NULL
+,nombre varchar(25)
+,apellido1 varchar(25)
+,apellido2 varchar(25)
+,rol varchar(25)
+,contraseña varchar(25)
+,email varchar(25)
+,puntacion number
+,id_proveedor nvarchar(10) not NULL
+
+, CONSTRAINT Usuario_pk primary key (id_usuario)
+, CONSTRAINT provedor_fk FOREIGN KEY (id_proveedor) REFERENCES proveedor 
+)
+
+
+create table Contratista(
+id_empresa varchar(10) not null
+,nombre varchar(25)
+,puntacion varchar (25)
+,email varchar (25)
+,contraseña varchar(25)
+,valor number
+,id_proveedor nvarchar(10) not NULL
+,id_usuario nvarchar(10)
+
+, CONSTRAINT Contratista_pk primary key (id_empresa)
+, CONSTRAINT provedor_fk FOREIGN KEY (id_proveedor) REFERENCES proveedor
+, usuario_fk FOREIGN KEY (id_usuario) REFERENCES Usuario  
+)
+
+create table Anteproyecto(
+id_ante_proyecto nvarchar(10) not NULL
+,Localizacion varchar(25)
+,descripcion varchar(25)
+,presupuesto number
+,duracionaprox varchar(25)
+,id_usuario nvarchar(10) not NULL
+,id_empresa varchar(10) not null
+
+,CONSTRAINT Contratista_pk primary key (id_ante_proyecto)
+,CONSTRAINT Usuario_fk FOREIGN KEY (id_usuario) REFERENCES Usuario 
+,CONSTRAINT empresa_fk FOREIGN KEY (id_empresa) REFERENCES Contratista
+)
+
 create table proyectos (
 id_proyecto nvarchar(10) not null
 ,descripcion varchar(50)
 ,duracion varchar(10)
 ,tipo varchar (5)
 ,localizacion varchar (10)
-,valor varchar (10)
+,valor number
 ,planofinal varchar (25)
+,id_ante_proyecto nvarchar(10) not NULL
+
 ,CONSTRAINT proyectos_pk primary key  (id_proyecto)
+,CONSTRAINT anteproyecto_fk FOREIGN KEY (id_ante_proyecto) REFERENCES Anteproyecto
 )
-create table Usuario(
-id_usuario nvarchar(10) not NULL
-,nombre varchar(25)
-,rol varchar(25)
-,contraseña varchar(25)
-,email varchar(25)
-,puntacion varchar(25)
-, CONSTRAINT Usuario_pk primary key (id_usuario)
-)
-
-create tabla proveedor(
-id_proveedor nvarchar(10) not NULL
-,nombre varchar(25)
-,email varchar (25)
-,fiabilidad varchar(25)
-,localizacion varchar(25)
-,producto varchar (25)
-,CONSTRAINT proveedor_pk primary key (id_proveedor)
-)
-
-create tabla Contratista(
-id_empresa varchar(10) not null
-,nombre varchar(25)
-,ountacion varchar (25)
-,email varchar (25)
-,contraseña 
-,valor
-, CONSTRAINT Contratista_pk primary key (id_empresa)
-)
-
-create table Anteproyecto(
-id_ante_proyecto nvarchar(10) no NULL
-,id_proyecto nvarchar(10) not Null
-,Localizacion 
-,descripcion varcahr (25)
-,presupuesto varchar(25)
-,duracionaprox varchar(25)
-,id_usuario nvarchar(10) not NULL
-,id_empresa varchar(10) not null
-CONSTRAINT Contratista_pk primary key (id_ante_proyecto)
-CONSTRAINT proyectos_p_fk FOREIGN KEY (id_proyecto) REFERENCES proyectos 
-CONSTRAINT proyectos_L_fk FOREIGN KEY (Localizacion) REFERENCES proyectos 
-CONSTRAINT Usuario_fk FOREIGN KEY (id_usuario) REFERENCES Usuario 
-CONSTRAINT empresa_fk FOREIGN KEY (id_empresa) REFERENCES Contratista
-)
-
